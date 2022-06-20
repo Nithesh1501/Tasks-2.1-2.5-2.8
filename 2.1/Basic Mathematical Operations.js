3c73d80d6af9e03cbf1ffec7d5df4d3d7482f978
Nithesh1501 <65076627+Nithesh1501@users.noreply.github.com>
2022-06-20 12:59:39 +0530

    Update Basic Mathematical Operations.js

--- 2.1/Basic Mathematical Operations.js
+++ 2.1/Basic Mathematical Operations.js
@@ -8,7 +8,18 @@ function basicOp(operation, value1, value2) {
         case '*':
             return value1 * value2;
         case '/':
-            return value1 / value2;
+            if(value1===0 && value2===0)
+            {
+                return NaN;
+            }
+            else if(value2===0)
+            {
+                return "Division is not possible"
+            }
+            else
+            {
+                return value1 / value2;
+            }
         default:
             return 0;
     }
